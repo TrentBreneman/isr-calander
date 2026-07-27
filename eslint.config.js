ee32ea471a27fca02de9620026d0d1c282e5ca1c
Trent Breneman <trentbreneman@icloud.com>
2026-07-27 16:03:14 -0600

    fix deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,30 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 import nextPlugin from "@next/eslint-plugin-next";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 
 export default [
   {
     ignores: [".next/**", "out/**", "node_modules/**", "deploy.cjs"],
   },
-  js.configs.recommended,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    languageOptions: {
-      parser: tsParser,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        React: "writable",
-      },
-    },
     plugins: {
       "@next/next": nextPlugin,
-      "@typescript-eslint": tsPlugin,
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs["core-web-vitals"].rules,
+      // your custom rules here
       "@next/next/no-img-element": "off",
-      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
 ];
