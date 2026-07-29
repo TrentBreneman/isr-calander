c522294ce6c1d686e19feee577e7820005e84e9c
Trent Breneman <trentbreneman@icloud.com>
2026-07-29 12:40:12 -0600

    Fix playwright-core bundling for serverless function

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  outputFileTracingIncludes: {
+    "/api/generate-pdf": [
+      "./node_modules/playwright-core/**",
+      "./node_modules/@sparticuz/chromium/**",
+    ],
+  },
 };
 
 export default nextConfig;
